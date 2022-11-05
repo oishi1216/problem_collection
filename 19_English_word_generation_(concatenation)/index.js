45695ada1b1b0d3f007b51254730afc3b2e01ec3
junji oishi <abdaqoopmhm@gmail.com>
2022-11-05 10:20:15 +0900

    paizaラーニング「英単語の生成（連結） JavaScript編」
    回答をコミット

---
+++ 19_English_word_generation_(concatenation)/index.js
@@ -0,0 +1,17 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    const data = lines[0].split(" ");
+    const answer = data[0] + data[1];
+    console.log(answer);
+});
