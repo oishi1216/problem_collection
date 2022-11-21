604da16821e555eb4debdc546d82a69b61103cfe
junji oishi <abdaqoopmhm@gmail.com>
2022-11-21 23:01:26 +0900

    paizaラーニング「指定された値の位置1 JavaScript編」
    回答をコミット

---
+++ 36_Position_of_the_specified_value_1/index.js
@@ -0,0 +1,17 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    const data = lines[1].split(" ").map(Number);
+    const answer = data.findIndex(num => num === Number(lines[2]));
+    console.log(answer + 1);
+});
