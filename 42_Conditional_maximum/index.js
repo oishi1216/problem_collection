f4da5de7369693835d8215de73a2b1018fdc2529
junji oishi <abdaqoopmhm@gmail.com>
2022-11-27 11:29:46 +0900

    paizaラーニング「条件付き最大値 JavaScript編」
    回答をコミット

---
+++ 42_Conditional_maximum/index.js
@@ -0,0 +1,26 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    const data = lines[1].split(" ").map(Number);
+    const arr = [];
+
+    data.forEach(num => {
+        if(num <= Number(lines[2])) {
+            arr.push(num);
+        }
+    })
+
+    arr.sort((a, b) => b - a);
+
+    console.log(arr[0]);
+});
